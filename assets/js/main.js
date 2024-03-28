fbbe4b51e4f17168da9c91cd0de10b2be6831403
OmarAliSoliman <omarasmar9955@gmail.com>
2024-03-29 00:21:05 +0200

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
     });
   }
 
-  if($(".brands_mobile_slider").length){
+  if ($(".brands_mobile_slider").length) {
     $(".brands_mobile_slider").slick({
       speed: 5000,
       infinite: true,
@@ -94,7 +94,7 @@ $(document).ready(function () {
       centerMode: false,
       autoplay: true,
       cssEase: "linear",
-    })
+    });
   }
 
   if ($(".custom_navbar").length) {
@@ -323,7 +323,6 @@ $(document).ready(function () {
   //   }
   // );
 
-
   // features_slider_mobile
   if ($(".features_slider_mobile").length) {
     $(".features_slider_mobile").slick({
@@ -365,7 +364,6 @@ $(document).ready(function () {
     });
   }
 
-
   // case_study_slider
   if ($(".case_study_slider").length) {
     $(".case_study_slider").slick({
@@ -382,20 +380,134 @@ $(document).ready(function () {
       focusOnSelect: false,
     });
   }
-  
+
+  if ($(".highprocess_card").length) {
+    var levelProcessAnimation = gsap.timeline();
+
+    $(".highprocess_card").map((index, item) => {
+      animationProcess(item);
+    });
+
+    function animationProcess(item) {
+      const halfCircle = $(item).find(".half_circle");
+      const outerCircle = $(item).find(".outer_circle");
+      const innerCircle = $(item).find(".inner_circle");
+      const icon = $(item).find(".icon");
+      const cardTitle = $(item).find(".card_body h5");
+      const cardText = $(item).find(".card_body p");
+      levelProcessAnimation
+        .from(halfCircle, { scale: 0, opacity: 0, duration: 0.3, ease: "ease" })
+        .from(outerCircle, {
+          scale: 0.4,
+          opacity: 0,
+          duration: 0.3,
+          ease: "ease",
+        })
+        .from(innerCircle, { opacity: 0, duration: 0.3, ease: "ease" })
+        .from(icon, { scale: 0.4, opacity: 0, duration: 0.3, ease: "ease" })
+        .from(cardTitle, { y: 10, opacity: 0, duration: 0.3, ease: "ease" })
+        .from(cardText, { y: 10, opacity: 0, duration: 0.3, ease: "ease" });
+    }
+
+    levelProcessAnimation.pause();
+
+    // Create an Intersection Observer
+    var observer = new IntersectionObserver(
+      function (entries) {
+        entries.forEach(function (entry) {
+          if (entry.isIntersecting) {
+            // If the 'time_line' section is in the viewport, start the animation
+            levelProcessAnimation.play();
+            observer.unobserve(entry.target); // Stop observing once animation starts
+          }
+        });
+      },
+      { threshold: 0.1 } // Adjust the threshold as needed
+    );
+
+    var timeLineSection = document.querySelector(".level_process");
+    observer.observe(timeLineSection);
+  }
 
   AOS.init();
 });
 
+const doc = document,
+  types = [
+    "circle",
+    "semi-circle",
+    "square",
+    "triangle",
+    "triangle-2",
+    "rectangle",
+  ],
+  colors = [
+    "#836ee5",
+    "#fe94b4",
+    "#49d2f5",
+    "#ff5354",
+    "#00b1b4",
+    "#ffe465",
+    "#0071ff",
+    "#03274b",
+  ];
+
+let shapes = doc.querySelectorAll(".from-left shape");
+shapes.forEach((shape, index) => {
+  setInterval(() => {
+    let cl = shape.classList;
+    shape.className = "";
+
+    //assign styles
+    cl.add(types[~~(Math.random() * types.length)]);
+    let offset = Math.random() * 4 - 2;
+    let opp = offset >= 0 ? "+ " : "- ";
+    let styles = [
+      ["left", "calc(50% " + opp + offset + "vw)"],
+      ["--bounce-variance", Math.random() * 20 - 10 + "vh"],
+      ["--base_scale", Math.random() * 6 + 4 + "vh"],
+      ["--rotation", Math.random() * 180 - 90 + "deg"],
+      ["--color", colors[~~(Math.random() * colors.length)]],
+    ];
+    styles.forEach((style) => shape.style.setProperty(style[0], style[1]));
+
+    //animate
+    if (!cl.contains("bounce-up")) cl.add("bounce-up");
+    cl.replace("bounce-down", "bounce-up");
+    setTimeout(() => cl.replace("bounce-up", "bounce-down"), 400);
+  }, 740);
+});
+
+let tl = gsap.timeline({ ease: "power4.inOut" });
+function animateFromLeft() {
+  tl.to(".from-left .tile", {
+    duration: 0.4,
+    width: "100%",
+    left: "0%",
+    delay: 0.2,
+    stagger: 0.05,
+  });
+  tl.to(".from-left shape", { opacity: 0,stagger:.5, ease: "ease" });
+  tl.to(".from-left .tile", {
+    duration: 0.4,
+    width: "100%",
+    left: "100%",
+    delay: 0.2,
+    stagger: -0.05,
+  });
+  tl.set(".from-left .tile", { left: "0", width: "0" });
+  // tl.to(".from-left", { display: "none" });
+}
+
 $(window).on("load", function () {
   var currentDir = $("body").css("direction");
-  // setTimeout(() => {
-  //   animateOut();
-  // }, 2000);
+  setTimeout(() => {
+    animateFromLeft();
+  }, 2000);
   new Mmenu("#menu", {
     offCanvas: {
       slidingSubmenus: false,
-      position: currentDir == "ltr" ? "left-front" : "right-front"
+      position: currentDir == "ltr" ? "left-front" : "right-front",
     },
     theme: "light",
     counters: {
